2009610cac6ff0b88c4385e7f87d6637761e2778
victorportugal8 <victorportugalnobrega@gmail.com>
2021-06-05 17:38:42 -0300

    corrigindo código

--- AULA10/HistoricoDisciplinas/GUIConsole/GUIConsole.cs
+++ AULA10/HistoricoDisciplinas/GUIConsole/GUIConsole.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Historico.GUIConsole{
+namespace HistoricoDisciplinas.GUIConsole{
     public class GUIConsole{
         public int Menu(){
             int opc;
