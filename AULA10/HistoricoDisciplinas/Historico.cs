9954ba990d86115819683946d52937d3ebd3e904
victorportugal8 <victorportugalnobrega@gmail.com>
2021-06-05 17:23:13 -0300

    arrumando codigo

--- AULA10/HistoricoDisciplinas/Historico.cs
+++ AULA10/HistoricoDisciplinas/Historico.cs
@@ -1,76 +1,52 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HistoricoDisciplinas{
-    class Historico{
-        private Disciplina[] vet;
-        private int qtd;
+    public class Historico : IEnumerable<Disciplina>{
+        private ICollection<Disciplina> vet;
 
         public Historico(){
-            vet = new Disciplina[100];
-            qtd = 0;
+            vet = new List<Disciplina>();
         }
 
         public void Inserir(Disciplina d){
-            vet[qtd] = d;
-            qtd++;
+            vet.Add(d);
         }
 
-        private int ObterIndice(int codigo){
-            int i = 0;
-            bool achou = false;
-
-            while(i < qtd && !achou){
-                if(vet[i].Codigo == codigo){
-                    achou = true;
-                } else{
-                    i++;
+        private Disciplina ObterDisciplina(int codigo){
+            foreach (Disciplina disc in vet){
+                if(disc.Codigo == codigo){
+                    return disc;
                 }
             }
-            return achou ? i : -1;
+            return null;
         }
 
         public void Remover(int codigo){
-            int pos = ObterIndice(codigo);
-            if(pos > -1){
-                for(int i = pos; i < qtd; i++){
-                    vet[i - 1] = vet[i];
-                }
-                qtd--;
+            Disciplina alvo = ObterDisciplina(codigo);
+
+            if(alvo != null){
+                vet.Remove(alvo);
             }
         }
 
         public void Alterar(int codigo, Disciplina d){
-            int pos = ObterIndice(codigo);
-            if(pos > -1){
-                vet[pos] = d;
+            Disciplina alvo = ObterDisciplina(codigo);
+
+            if(alvo != null){
+                vet.Add(d);
+                vet.Remove(alvo);
             }
         }
 
-        public void Listar(){
-            Console.WriteLine("{0, -30}{1}{2}", "Nome", "Cred", "Media");
-            for(int i = 0; i < qtd; i++){
-                Console.WriteLine("{0, -30} {1:00} {2:00.00}", vet[i].Nome, vet[i].Creditos, vet[i].Media());
-            }
+        public IEnumerator<Disciplina> GetEnumerator(){
+            return vet.GetEnumerator();
         }
 
-        public void ListarTipo2(){
-            Console.WriteLine("{0, -30}{1}{2}", "Nome", "Cred", "Media");
-            for(int i = 0; i < qtd; i++){
-                if(vet[i] is DisciplinaTipo2){
-                    Console.WriteLine("{0, -30} {1:00} {2:00.00}", vet[i].Nome, vet[i].Creditos, vet[i].Media());
-                }
-            }
-        }
-
-        public void ListarTipo3(){
-            Console.WriteLine("{0, -30}{1}{2}", "Nome", "Cred", "Media");
-            for(int i = 0; i < qtd; i++){
-                if(vet[i] is DisciplinaTipo3 && (vet[i] as DisciplinaTipo3).NotaApresentacao > 7){
-                    Console.WriteLine("{0, -30} {1:00} {2:00.00}", vet[i].Nome, vet[i].Creditos, vet[i].Media());
-                }
-            }
+        IEnumerator IEnumerable.GetEnumerator(){
+            return vet.GetEnumerator();
         }
     }
 }
