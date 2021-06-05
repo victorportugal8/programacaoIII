f893c3523146825fb6115d01f9e9aa37ebb00607
victorportugal8 <victorportugalnobrega@gmail.com>
2021-06-05 17:42:29 -0300

    Corrigindo código

--- AULA10/HistoricoDisciplinas/HistoricoDisciplinas.cs
+++ AULA10/HistoricoDisciplinas/HistoricoDisciplinas.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 
 namespace HistoricoDisciplinas{
-    public static void Main(String []args){
-        Historico h = new Historico();
-        Disciplina d;
-        GUITosca.GUITosca gui = new GUITosca.GUITosca();
+    class HistoricoDisciplinas{
+        public static void Main(String []args){
+            Historico h = new Historico();
+            Disciplina d;
+            GUITosca.GUITosca gui = new GUITosca.GUITosca();
 
-        int codigo;
-        bool fim = false;
+            int codigo;
+            bool fim = false;
 
-        while(!fim){
-            switch(gui.Menu()){
-                case 1:
-                    d = new Disciplina();
-                    gui.Ler(d);
-                    h.Inserir(d);
-                    break;
-                case 2:
-                    codigo = gui.LerCodigo("Remover");
-                    h.Remover(codigo);
-                    break;
-                case 3:
-                    codigo = gui.LerCodigo("Alterar");
-                    h.Remover(codigo);
-                    d = new Disciplina();
-                    gui.Ler(d);
-                    h.Alterar(codigo, d);
-                    break;
-                case 4:
-                    gui.Listar(h);
-                    break;
-                case 5:
-                    fim = true;
-                    break;
+            while(!fim){
+                switch(gui.Menu()){
+                    case 1:
+                        d = new Disciplina();
+                        gui.Ler(d);
+                        h.Inserir(d);
+                        break;
+                    case 2:
+                        codigo = gui.LerCodigo("Remover");
+                        h.Remover(codigo);
+                        break;
+                    case 3:
+                        codigo = gui.LerCodigo("Alterar");
+                        h.Remover(codigo);
+                        d = new Disciplina();
+                        gui.Ler(d);
+                        h.Alterar(codigo, d);
+                        break;
+                    case 4:
+                        gui.Listar(h);
+                        break;
+                    case 5:
+                        fim = true;
+                        break;
+                }
             }
-        }
+        }    
     }
 }
